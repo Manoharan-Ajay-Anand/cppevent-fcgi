bc9d8ae93f1f94898dbcc908254e3c69201fe059
Manoharan-Ajay-Anand <ajayanandm@gmail.com>
2023-08-19 20:47:10 +0800

    update handler

--- src/cppevent_fcgi/fcgi_handler.cpp
+++ src/cppevent_fcgi/fcgi_handler.cpp
@@ -6,19 +6,42 @@
 #include <cppevent_base/util.hpp>
 
 #include <cstdint>
-#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <vector>
 #include <iostream>
 
-cppevent::awaitable_task<long> get_length(cppevent::stream& s) {
+cppevent::awaitable_task<std::pair<long, long>> get_lengths(cppevent::stream& s) {
+    long result[2];
     uint8_t data[4];
-    co_await s.read(data, 1);
-    if ((data[0] >> 7) == 0) {
-        co_return { data[0] };
+    for (int i = 0; i < 2; ++i) {
+        co_await s.read(data, 1);
+        if ((data[0] >> 7) == 0) {
+            result[i] = data[0];
+            continue;
+        }
+        data[0] = data[0] & 0x7f;
+        co_await s.read(data + 1, 3);
+        result[i] = cppevent::read_u32_be(data);
     }
-    data[0] = data[0] & 0x7f;
-    co_await s.read(data + 1, 3);
-    co_return { cppevent::read_u32_be(data) };
+    co_return { result[0], result[1] };
 }
+
+struct header {
+    std::vector<char>& header_buf;
+    long name_offset;
+    long name_len;
+    long val_offset;
+    long val_len;
+
+    std::string_view get_name() const {
+        return { header_buf.data() + name_offset, static_cast<std::size_t>(name_len) };
+    }
+
+    std::string_view get_value() const {
+        return { header_buf.data() + val_offset, static_cast<std::size_t>(val_len) };
+    }
+};
 
 cppevent::awaitable_task<void> cppevent::fcgi_handler::handle_request(stream& s_params,
                                                                       stream& s_stdin,
@@ -26,15 +49,26 @@ cppevent::awaitable_task<void> cppevent::fcgi_handler::handle_request(stream& s_
                                                                       output& o_endreq,
                                                                       output_queue& o_queue,
                                                                       bool close_conn) {
+    std::unordered_map<std::string_view, std::string_view> header_map;
+    std::vector<char> header_buf;
+    std::vector<header> headers;
+    header_buf.reserve(1024);
+    long h_offset = 0;
     while ((co_await s_params.can_read())) {
-        long name_l = co_await get_length(s_params);
-        long val_l = co_await get_length(s_params);
-        std::string name(name_l, '\0');
-        std::string val(val_l, '\0');
-        co_await s_params.read(name.data(), name_l);
-        co_await s_params.read(val.data(), val_l);
-        std::cout << name << ": " << val << std::endl;  
+        auto [name_l, val_l] = co_await get_lengths(s_params);
+        long total_l = name_l + val_l;
+        header_buf.resize(header_buf.size() + total_l);
+        co_await s_params.read(header_buf.data() + h_offset, total_l);
+        headers.push_back({ header_buf, h_offset, name_l, h_offset + name_l, val_l });
+        h_offset += total_l;
     }
+    for (const auto& header : headers) {
+        header_map[header.get_name()] = header.get_value();
+    }
+    for (auto& p : header_map) {
+        std::cout << p.first << ": " << p.second << std::endl;
+    }
+    std::cout << std::endl;
     co_await o_stdout.write("content-length: 5\n");
     co_await o_stdout.write("content-type: text/plain\n\nhello");
     co_await o_stdout.end();
